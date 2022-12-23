9decfc4adbf50fc621a505e9ce5975fae1ca0b35
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2022-12-23 09:56:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var name = window.prompt("What is your name?");
+
+var age = window.prompt("What is your age?");     //To get user input
+document.write("Hey " + name + " You are " + age + " Years");
